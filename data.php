304672f178af4b258a5c7ad78c9c638e25b98e6b
Fahrel Gibran Alghany <fahrel@students.undip.ac.id>
2022-08-29 18:54:51 +0700

    first commit

---
+++ data.php
@@ -0,0 +1,46 @@
+<?php
+
+$questions = [
+  1 => [ 
+    'question' => 'Siapa hokage pertama Konoha?', 
+    'options' => 
+    [
+      'a' => 'Naruto', 
+      'b' => 'Sasuke', 
+      'c' => 'Luffy', 
+      'd' => 'Ichigo', 
+      'e' => 'Jokowi'
+    ],
+    'answer' => 'e'
+  ],
+  2 => [
+    'question' => 'Siapa raja bajak laut?', 
+    'options' => 
+    [
+      'a' => 'Roger', 
+      'b' => 'Sasuke', 
+      'c' => 'Luffy', 
+      'd' => 'Ichigo', 
+      'e' => 'Jokowi'
+    ],
+    'answer' => 'a'
+  ],
+  3 => [
+    'question' => 'Siapa raja bajak laut?', 
+    'options' => 
+    [
+      'a' => 'Roger', 
+      'b' => 'Sasuke', 
+      'c' => 'Luffy', 
+      'd' => 'Ichigo', 
+      'e' => 'Jokowi'
+    ],
+    'answer' => 'a'
+  ],
+];
+
+$answers = [
+  1 => 'e',
+  2 => 'a'
+];
+
---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+// Function to calculate how many questions are correct
+function calculateScore($answers, $questions) {
+  $score = 0;
+  // Loop in foreach question
+  foreach ($questions as $key => $question) {
+    // If there is no answer for the question, then skip it
+    if (!isset($answers[$key])) {
+      continue;
+    }
+    // If the answer is correct, add 1 to the score
+    if ($answers[$key] == $question['answer']) {
+      $score++;
+    }
+  }
+  // Count how many questions
+  $total = count($questions);
+  // Calculate the percentage of correct answers
+  $percentage = $score / $total * 100;
+  // Round the percentage to 2 decimal places
+  $percentage = round($percentage, 2);
+  // Return the percentage
+  return $percentage;
+}
